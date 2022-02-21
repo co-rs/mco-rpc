c084994f35f87f66f34d8b2d7d60b4cafc4d62fd
zxj <zhuxiujia@qq.com>
2022-02-21 14:43:46 +0800

    add stub

--- src/codec.rs
+++ src/codec.rs
@@ -1,8 +1,50 @@
-use mco::mco_gen::Error;
+use mco::err;
+use mco::std::errors::Error;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 
 pub trait Codec {
     fn encode<T: Serialize>(&self, arg: T) -> Result<Vec<u8>, Error>;
     fn decode<T: DeserializeOwned>(&self, arg: &[u8]) -> Result<T, Error>;
+}
+
+
+pub struct JsonCodec {}
+
+impl Codec for JsonCodec {
+    fn encode<T: Serialize>(&self, arg: T) -> Result<Vec<u8>, Error> {
+        match serde_json::to_vec(&arg) {
+            Ok(ok) => { Ok(ok) }
+            Err(e) => { Err(err!("{}",e)) }
+        }
+    }
+
+    fn decode<T: DeserializeOwned>(&self, arg: &[u8]) -> Result<T, Error> {
+        match serde_json::from_slice(arg) {
+            Ok(v) => {
+                Ok(v)
+            }
+            Err(e) => {
+                Err(err!("{}",e))
+            }
+        }
+    }
+}
+
+pub struct BinCodec {}
+
+impl Codec for BinCodec{
+    fn encode<T: Serialize>(&self, arg: T) -> Result<Vec<u8>, Error> {
+        match bincode::serialize(&arg) {
+            Ok(ok) => { Ok(ok) }
+            Err(e) => { Err(err!("{}",e)) }
+        }
+    }
+
+    fn decode<T: DeserializeOwned>(&self, arg: &[u8]) -> Result<T, Error> {
+        match bincode::deserialize(arg) {
+            Ok(ok) => { Ok(ok) }
+            Err(e) => { Err(err!("{}",e)) }
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 extern crate serde;
 extern crate mco;
 
+#[macro_use]
+extern crate serde_json;
+#[macro_use]
+extern crate bincode;
+
 pub mod codec;
 pub mod stub;
 pub mod proto;
