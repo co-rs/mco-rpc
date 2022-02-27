d316aa71fe4bd4917e5e4c5fb952c4e2ccb05eb0
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-02-28 02:41:48 +0800

    add bench test

---
+++ benches/latency.rs
@@ -0,0 +1,52 @@
+#![feature(test)]
+#[macro_use]
+extern crate mco_rpc;
+
+#[cfg(test)]
+extern crate test;
+extern crate mco;
+
+use std::net::SocketAddr;
+use std::time::Duration;
+#[cfg(test)]
+use test::Bencher;
+use mco::coroutine::{sleep, spawn};
+use mco_rpc::client::Client;
+use mco_rpc::codec::{Codecs, JsonCodec};
+use mco_rpc::server::{Handler, Server};
+
+
+pub struct H {}
+
+impl Handler for H {
+    type Req = i32;
+    type Resp = i32;
+
+    fn handle(&self, req: Self::Req) -> mco::std::errors::Result<Self::Resp> {
+        Ok(req)
+    }
+}
+
+
+
+#[cfg(test)]
+#[bench]
+fn latency(bencher: &mut Bencher) {
+    spawn(move ||{
+        let mut s = Server::default();
+        //s.codec = Codecs::JsonCodec(JsonCodec{});
+        s.register("handle",H {});
+        s.serve(("0.0.0.0", 10000));
+        println!("rpc served");
+    });
+    sleep(Duration::from_secs(1));
+    let mut c = Client::dial(SocketAddr::from(([127, 0, 0, 1], 10000))).unwrap();
+    //c.codec = Codecs::JsonCodec(JsonCodec{});
+    println!("dial success");
+    let resp:i32 = c.call("handle",1).unwrap();
+    println!("resp=>>>>>>>>>>>>>> :{}",resp);
+
+    bencher.iter(||{
+        let resp:i32 = c.call("handle",1).unwrap();
+    });
+}
--- src/stub.rs
+++ src/stub.rs
@@ -62,54 +62,10 @@ impl ClientStub {
                 info!("get response id = {}", id);
                 let rsp_req = rsp_frame.decode_req();
                 let rsp_data = rsp_frame.decode_rsp().unwrap();
-                println!("client req get= {}", String::from_utf8(rsp_req.to_vec()).unwrap_or_default());
-                println!("client rsp get= {}", String::from_utf8(rsp_data.to_vec()).unwrap_or_default());
-
                 let resp: Resp = codec.decode(rsp_data)?;
                 return Ok(resp);
             }
         }
-
-        // let req = codec.encode(arg)?;
-        // let header = PackHeader { m: method.to_string(), t: self.tag.load(Ordering::SeqCst) + 1, l: req.len() };
-        // let header_data = codec.encode(header)?;
-        // println!("header:{}", String::from_utf8(header_data.clone()).unwrap_or_default());
-        // println!("body:{}", String::from_utf8(req.clone()).unwrap_or_default());
-        // stream.write_all(&header_data);
-        // stream.write_all("\n".as_bytes());//write eof
-        // println!("header_len:{}", header_data.len());
-        // stream.write_all(&req);
-        // stream.write_all("\n".as_bytes());//write eof
-        // stream.flush()?;
-        // let mut buf_header = {
-        //     let mut buf = Vec::with_capacity(4096);
-        //     for _ in 0..4096 {
-        //         buf.push(0);
-        //     }
-        //     buf
-        // };
-        // loop {
-        //     reset(&mut buf_header);
-        //     let read_len = stream.read_to_end(&mut buf_header)?;
-        //     if read_len != 0 {
-        //         println!("header-read len:{}", read_len);
-        //         let buf_header = &buf_header[0..read_len];
-        //         println!("header-resp:{}", String::from_utf8(buf_header.to_vec()).unwrap_or_default());
-        //         if let Ok(h) = codec.decode::<PackHeader>(&buf_header) {
-        //             let mut buf = {
-        //                 let mut buf = Vec::with_capacity(h.l);
-        //                 for _ in 0..h.l {
-        //                     buf.push(0);
-        //                 }
-        //                 buf
-        //             };
-        //             let read_len = stream.read(&mut buf)?;
-        //             println!("body-resp:{}", String::from_utf8(buf.clone()).unwrap_or_default());
-        //             let resp: Resp = codec.decode(&buf)?;
-        //             return Ok(resp);
-        //         }
-        //     }
-        // }
     }
 }
 
